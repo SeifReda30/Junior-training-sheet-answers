1599e141ef50fc434fe962385121d1dcbb098aa6
Seif Reda <69864768+SeifReda30@users.noreply.github.com>
2022-09-01 14:30:22 +0200

    Update

--- iq test/main.cpp
+++ iq test/main.cpp
@@ -1,41 +1,43 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
-
 int main()
 {
-    int n,i,c,z;
-    int even=0;
-    int odd=0;
-    int arr[100];
-    cin>>n;
-    if (n>=3&&n<=100)
+    char a[4][4];
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            cin>>a[i][j];
+    int n=4;
+    int cont=0;
+    for(int i=0; i<n-1; i++)
     {
-    for (i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    for (i=0;i<n;i++)
-    {
-        if(arr[i]%2==0)
+        for(int j=0; j<n-1; j++)
         {
-            even++;
-            c=i+1;
+            int has=0,dot=0;
+            if(a[i][j]=='#')
+                has++;
+            else
+                dot++;
+            if(a[i+1][j]=='#')
+                has++;
+            else
+                dot++;
+            if(a[i][j+1]=='#')
+                has++;
+            else
+                dot++;
+            if(a[i+1][j+1]=='#')
+                has++;
+            else
+                dot++;
+            if(has==3||dot==3||has==4||dot==4)
+                {
+                    cont++;
+                    cout<<"YES";
+                    return 0;
+                }
         }
-        else
-        {
-            odd++;
-            z=i+1;
-        }
     }
-    if(even>odd)
-    {
-        cout<<z;
-    }
-    else
-    {
-        cout<<c;
-    }
-    }
-    return 0;
+    if(cont==0)
+        cout<<"NO";
 }
